d78243db62d1ae4fe31abd09aed2da1d7b9a16a8
alexkhazzam <akhazzam1@student.gn.k12.ny.us>
2021-01-16 15:30:42 -0500

    init commit

---
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "BullCowCartridge.h"
+
+void UBullCowCartridge::BeginPlay() // When the game starts
+{
+    Super::BeginPlay();
+    PrintLine(TEXT("Welcome to Bull Cows!"));
+    // TODO Remove Magic Number
+    PrintLine(TEXT("Guess the 4 letter word"));
+    PrintLine(TEXT("Press enter to continue..."));
+    HiddenWord = TEXT("cake");
+}
+
+void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
+{
+    ClearScreen();
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("You have Won!"));
+    }
+    else
+    {
+        PrintLine(TEXT("You have Lost!"));
+    }
+}
